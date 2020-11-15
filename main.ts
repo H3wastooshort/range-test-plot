b7428f2e8e5196ab69d9daa49750f84d9ee745c9
HACKER3000 <hacker3000@posteo.org>
2020-11-15 19:43:46 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,48 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (!(transmit)) {
+        led.plotBarGraph(
+        radio.receivedPacket(RadioPacketProperty.SignalStrength) + 100,
+        40
+        )
+        music.playTone(988, music.beat(BeatFraction.Sixteenth))
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (power > 0) {
+        power += -1
+        radio.setTransmitPower(power)
+    }
+    basic.showNumber(power)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    transmit = true
+})
+input.onButtonPressed(Button.B, function () {
+    if (power < 7) {
+        power += 1
+        radio.setTransmitPower(power)
+    }
+    basic.showNumber(power)
+    basic.clearScreen()
+})
+let transmit = false
+let power = 0
+radio.setTransmitPower(7)
+radio.setFrequencyBand(62)
+radio.setGroup(165)
+power = 7
+transmit = false
+basic.forever(function () {
+    if (transmit) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            # . . . #
+            . . # . .
+            . # . # .
+            `)
+        radio.sendNumber(control.deviceSerialNumber())
+        basic.clearScreen()
+    }
+})
